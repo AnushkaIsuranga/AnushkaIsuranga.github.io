f3a517a24b7f24676c070154c0f17a20aae198f4
AnushkaIsuranga <isuranga880@hotmail.com>
2024-08-31 16:58:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/AnushkaIsuranga-github-io/"
+  base: "/AnushkaIsuranga-github-io/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
